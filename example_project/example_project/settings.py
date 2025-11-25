5e092b7b37ed2c999665e27bd3c0f255382fce26
Shaun Flagg <shaun.flagg@fastly.com>
2025-11-25 10:26:11 -0500

    Use DATABASE_URL for Postgres with SQLite fallback

--- example_project/example_project/settings.py
+++ example_project/example_project/settings.py
@@ -1,5 +1,7 @@
 import os
 from pathlib import Path
+import dj_database_url
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -51,11 +53,13 @@ TEMPLATES = [
 WSGI_APPLICATION = "example_project.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
 
 AUTH_PASSWORD_VALIDATORS = []
 
